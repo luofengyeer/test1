01818e4f88142ce777f90e760a75fbbaf03cc61e
gy <476266546@qq.com>
2018-12-12 21:25:37 +0800

    修改黄页内容显示

--- app/src/main/java/com/cmcc/internalcontact/activity/YellowPageActivity.java
+++ app/src/main/java/com/cmcc/internalcontact/activity/YellowPageActivity.java
@@ -2,6 +2,7 @@ package com.cmcc.internalcontact.activity;
 
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.TextView;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import com.cmcc.internalcontact.R;
 import com.cmcc.internalcontact.base.BaseActivity;
 import com.cmcc.internalcontact.utils.Constant;
+import com.cmcc.internalcontact.utils.SharePreferencesUtils;
 import com.cmcc.internalcontact.utils.Utils;
 
 import butterknife.BindView;
@@ -28,7 +30,9 @@ public class YellowPageActivity extends BaseActivity {
         wvYellowPage.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
         wvYellowPage.getSettings().setLoadWithOverviewMode(true);
         wvYellowPage.getSettings().setAllowFileAccess(true);
-        wvYellowPage.loadUrl(Constant.YELLOW_PAGE_URL);
+        String string = SharePreferencesUtils.getInstance().getString(Constant.YELLOW_PAGE, "");
+        Log.d("GetYellowPageContent", string);
+        wvYellowPage.loadData(string, "text/html", "utl-8");
     }
 
     private void buildTel(TextView textView) {
